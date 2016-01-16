97dddd4040d0dfc98f5fc3c79940795c2f175f86
Tak Sampson <tak.sampson@gmail.com>
2016-01-16 18:07:26 -0500

    rubocop calculator

--- calculator.rb
+++ calculator.rb
@@ -5,33 +5,38 @@ def prompt(message)
   Kernel.puts("=> #{message}")
 end
 
-
 def to_i_or_f(num)
-  if num == num.to_i 
-    num.to_i 
+  if num == num.to_i
+    num.to_i
   else
     num
   end
 end
 
-def whitelist_array(message,arr)
-  prompt(message)
-  input = Kernel.gets().chomp()
-  unless arr.include?(input)
-    prompt("Invalid entry. Please try again.")
-    input_whitelist(message,arr)
+def whitelist_array(message, arr)
+  input = ''
+  loop do
+    prompt(message)
+    input = Kernel.gets().chomp
+    if !arr.include?(input)
+      prompt("Invalid entry. Please try again.")
+    else break
+    end
   end
   input
 end
 
 def whitelist_realnum(message)
-  Kernel.puts(message)
-  input = Kernel.gets().chomp
-  unless (input == input.to_f.to_s) || (input == input.to_i.to_s) # string must be "#{integer or float}"
-    Kernel.puts("=> Not a real number. Please try again.")
-    whitelist_realnum(message)
+  input = ''
+  loop do
+    prompt(message)
+    input = Kernel.gets().chomp
+    if (input != input.to_f.to_s) && (input != input.to_i.to_s) # string must be "#{integer or float}"
+      prompt("Not a real number. Please try again.")
+    else break
+    end
   end
-  input 
+  input
 end
 
 def operation_to_message(op)
@@ -47,9 +52,8 @@ def operation_to_message(op)
   end
 end
 
-
 Kernel.puts("Welcome to Calculator!")
-loop do 
+loop do
   prompt("Enter your name:")
   name = Kernel.gets().chomp()
   unless name.empty?
@@ -60,7 +64,6 @@ loop do
 end
 
 loop do # main loop
-
   num1 = whitelist_realnum("What's the first number?")
   num2 = whitelist_realnum("What's the second number?")
 
@@ -82,31 +85,29 @@ loop do # main loop
     m) multiply
   MSG
 
-  if to_i_or_f(num2) != 0
-    operation = whitelist_array(operator_prompt, ['a','s','m','d'])
+  if num2.to_f != 0
+    operation = whitelist_array(operator_prompt, %w(a s m d))
   else
-    operation = whitelist_array(divide_by_zero_prompt, ['a','s','m'])
+    operation = whitelist_array(divide_by_zero_prompt, %w(a s m))
   end
 
   prompt("#{operation_to_message(operation)} the two numbers...")
 
   result = case operation
            when 'a'
-            to_i_or_f(num1.to_f + num2.to_f)
+             to_i_or_f(num1.to_f + num2.to_f)
            when 's'
-            to_i_or_f(num1.to_f - num2.to_f)
+             to_i_or_f(num1.to_f - num2.to_f)
            when 'm'
-            to_i_or_f(num1.to_f * num2.to_f)
+             to_i_or_f(num1.to_f * num2.to_f)
            when 'd'
-            to_i_or_f(num1.to_f / num2.to_f)
-  end
+             to_i_or_f(num1.to_f / num2.to_f)
+           end
 
-  prompt("The answer is #{result.to_s}")
+  prompt("The answer is #{result}")
 
   prompt("Do you wish to perform another calculation? (press y to calculate again)")
   answer = Kernel.gets().chomp
   break unless answer.downcase == 'y'
-
 end
-  prompt("Thank you for using Calculator!")
-
+prompt("Thank you for using Calculator!")
